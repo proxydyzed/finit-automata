3d6d3ccce2cd3b260521a11d477644bc8f7c70af
proxydyzed <tonnoysarker2@gmail.com>
2025-11-14 20:56:27 +0600

    another test for the subset constructor

---
+++ tests/test-subset-2.js
@@ -0,0 +1,45 @@
+import { FixedColumnTable } from "./tables.js";
+import { NondeterministicFiniteAutomata, KnownMappings } from "./nfa.js";
+import { subsetConstruction } from "./subset-construction.js";
+
+try {
+  const nfa = new NondeterministicFiniteAutomata("n0");
+
+  // NFA for the RE "a(b|c)"
+  const n0 = nfa.start;
+  const n1 = nfa.addVertex("n1");
+  const n2 = nfa.addVertex("n2");
+  const n3 = nfa.addVertex("n3");
+  const n4 = nfa.addVertex("n4");
+  const n5 = nfa.addVertex("n5");
+  const n6 = nfa.addVertex("n6");
+  const n7 = nfa.addVertex("n7");
+  const n8 = nfa.addVertex("n8");
+  const n9 = nfa.addVertex("n9");
+
+  const a = nfa.addAlphabet("a");
+  const b = nfa.addAlphabet("b");
+  const c = nfa.addAlphabet("c");
+
+  nfa.addEdge(a, n0, n1);
+  nfa.addEdge(KnownMappings.epsilon, n1, n2);
+  nfa.addEdge(KnownMappings.epsilon, n2, n3);
+  nfa.addEdge(KnownMappings.epsilon, n2, n9);
+  nfa.addEdge(KnownMappings.epsilon, n3, n4);
+  nfa.addEdge(KnownMappings.epsilon, n3, n6);
+  nfa.addEdge(b, n4, n5);
+  nfa.addEdge(KnownMappings.epsilon, n5, n8);
+  nfa.addEdge(c, n6, n7);
+  nfa.addEdge(KnownMappings.epsilon, n7, n8);
+  nfa.addEdge(KnownMappings.epsilon, n8, n3);
+  nfa.addEdge(KnownMappings.epsilon, n8, n9);
+
+  // console.log(JSON.stringify(nfa, null, 4));
+  // console.log(nfa.stringifyMappings());
+
+  const data = subsetConstruction(nfa);
+  console.log("Entries:", data.entries);
+  console.log(`Table{ rows: ${data.table.rows}, cols: ${data.table.cols} }:`, data.table.buffer);
+} catch (error) {
+  console.error(error);
+}
