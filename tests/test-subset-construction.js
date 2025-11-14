45f89f484fc86a8f7ff81cf8b6148400a8c8b6ba
proxydyzed <tonnoysarker2@gmail.com>
2025-11-14 19:27:24 +0600

    probably should have done this earlier

--- tests/test-subset-construction.js
+++ tests/test-subset-construction.js
@@ -1,60 +1,59 @@
 import { FixedColumnTable } from "./tables.js";
 import { NondeterministicFiniteAutomata, KnownMappings } from "./nfa.js";
 
-const nfa = new NondeterministicFiniteAutomata("q0");
+try {
+  const nfa = new NondeterministicFiniteAutomata("q0");
 
-const q0 = nfa.start;
-const q1 = nfa.addVertex("q1");
-const q2 = nfa.addVertex("q2");
-const q3 = nfa.addVertex("q3");
-const q4 = nfa.addVertex("q4");
+  const q0 = nfa.start;
+  const q1 = nfa.addVertex("q1");
+  const q2 = nfa.addVertex("q2");
+  const q3 = nfa.addVertex("q3");
+  const q4 = nfa.addVertex("q4");
 
-const a = nfa.addAlphabet("a");
-const b = nfa.addAlphabet("b");
+  const a = nfa.addAlphabet("a");
+  const b = nfa.addAlphabet("b");
 
-nfa.addEdge(a, q0, q1);
-nfa.addEdge(KnownMappings.epsilon, q0, q3);
-nfa.addEdge(b, q1, q2);
-nfa.addEdge(KnownMappings.sigma, q2, q0);
-nfa.addEdge(KnownMappings.sigma, q3, q4);
-nfa.addEdge(b, q4, q3);
+  nfa.addEdge(a, q0, q1);
+  nfa.addEdge(KnownMappings.epsilon, q0, q3);
+  nfa.addEdge(b, q1, q2);
+  nfa.addEdge(KnownMappings.sigma, q2, q0);
+  nfa.addEdge(KnownMappings.sigma, q3, q4);
+  nfa.addEdge(b, q4, q3);
 
-// console.log(JSON.stringify(nfa, null, 4));
-console.log(nfa.stringifyMappings());
-
-const table = new FixedColumnTable(nfa.alphabets.size);
-// console.log(Array.from(epsilonClosure(nfa, [nfa.start]), state => state.description));
-const reversed = new Map(Array.from(nfa.alphabets, ([v, k]) => [k, v]));
-reversed.set(KnownMappings.epsilon, "∈");
-reversed.set(KnownMappings.sigma, "∑");
-
-const qq = epsilonClosure(nfa, [nfa.start]);
-const Q = new Set(qq);
-const worklist = [qq];
-
-while (worklist.length > 0) {
-  const q = worklist.pop();
-  console.log("worklist", Array.from(q, s => s.description));
-  
-  for (const index of nfa.alphabets.values()) {
-    const d = deltas(nfa, q, index);
-    const t = epsilonClosure(nfa, d);
-    console.log(`For "${reversed.get(index)}"\n`,
-      "Delta", Array.from(d, s => s.description), "\n",
-      "epsilon-closure", Array.from(t, s => s.description),
-    );
-  }
-  
+  // console.log(JSON.stringify(nfa, null, 4));
+  console.log(nfa.stringifyMappings());
+  subsetConstruction(nfa);
+} catch (error) {
+  console.error(error);
 }
 
+/**
+ * @param {NondeterministicFiniteAutomata} nfa
+ */
+function subsetConstruction(nfa) {
+  const table = new FixedColumnTable(nfa.alphabets.size);
+  const reversed = new Map(Array.from(nfa.alphabets, ([v, k]) => [k, v]));
+  reversed.set(KnownMappings.epsilon, "∈");
+  reversed.set(KnownMappings.sigma, "∑");
 
+  const qq = epsilonClosure(nfa, [nfa.start]);
+  const Q = new Set(qq);
+  const worklist = [qq];
 
-
-
-
-
-
-
+  while (worklist.length > 0) {
+    const q = worklist.pop();
+    console.log("worklist", Array.from(q, s => s.description));
+    
+    for (const index of nfa.alphabets.values()) {
+      const d = deltas(nfa, q, index);
+      const t = epsilonClosure(nfa, d);
+      console.log(`For "${reversed.get(index)}"\n`,
+        "Delta", Array.from(d, s => s.description), "\n",
+        "epsilon-closure", Array.from(t, s => s.description),
+      );
+    }
+  }
+}
 
 /**
  * @param {NondeterministicFiniteAutomata} nfa
