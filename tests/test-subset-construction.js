ec24b7b02840acf06ff57478d38112ae095a22da
proxydyzed <tonnoysarker2@gmail.com>
2025-11-14 20:10:13 +0600

    a working version of subset construction, might needs some more work

--- tests/test-subset-construction.js
+++ tests/test-subset-construction.js
@@ -21,8 +21,11 @@ try {
   nfa.addEdge(b, q4, q3);
 
   // console.log(JSON.stringify(nfa, null, 4));
-  console.log(nfa.stringifyMappings());
-  subsetConstruction(nfa);
+  // console.log(nfa.stringifyMappings());
+
+  const data = subsetConstruction(nfa);
+  console.log("Entries:", data.entries);
+  console.log(`Table{ rows: ${data.table.rows}, cols: ${data.table.cols} }:`, data.table.buffer);
 } catch (error) {
   console.error(error);
 }
@@ -31,28 +34,70 @@ try {
  * @param {NondeterministicFiniteAutomata} nfa
  */
 function subsetConstruction(nfa) {
-  const table = new FixedColumnTable(nfa.alphabets.size);
   const reversed = new Map(Array.from(nfa.alphabets, ([v, k]) => [k, v]));
   reversed.set(KnownMappings.epsilon, "∈");
   reversed.set(KnownMappings.sigma, "∑");
 
+  const table = new FixedColumnTable(nfa.alphabets.size);
   const qq = epsilonClosure(nfa, [nfa.start]);
   const Q = new Set(qq);
   const worklist = [qq];
+  let worklistIndex = 0;
 
-  while (worklist.length > 0) {
-    const q = worklist.pop();
-    console.log("worklist", Array.from(q, s => s.description));
+  while (worklist.length > worklistIndex) {
+    const q = worklist.at(worklistIndex);
+    worklistIndex++;
+
+    // console.log("worklist", Array.from(q, s => s.description));
     
+    let col = 0;
+    const row = table.rows;
+    table.allocRow();
     for (const index of nfa.alphabets.values()) {
       const d = deltas(nfa, q, index);
       const t = epsilonClosure(nfa, d);
-      console.log(`For "${reversed.get(index)}"\n`,
-        "Delta", Array.from(d, s => s.description), "\n",
-        "epsilon-closure", Array.from(t, s => s.description),
-      );
+
+      // console.log(`For "${reversed.get(index)}"\n`,
+      //   "Delta", Array.from(d, s => s.description), "\n",
+      //   "epsilon-closure", Array.from(t, s => s.description),
+      // );
+
+      const elem = table.get({ row: row, col: col });
+      elem.deref = t;
+
+      // if (!subsetOf(t, Q)) {
+      //   addTo(t, Q);
+      //   worklist.push(t);
+      // }
+
+      if (t.size > 0) {
+        const alreadyProcessed = worklist.some(workset => {
+          if (workset.size !== t.size) {
+            return false;
+          }
+
+          for (const elem of workset) {
+            if (!t.has(elem)) {
+              return false;
+            }
+          }
+
+          return true;
+        });
+
+        if (!alreadyProcessed) {
+          worklist.push(t);
+        }
+      }
+      col++;
     }
+
+    // console.log("-- end --\n\n");
   }
+  return {
+    entries: worklist,
+    table: table,
+  };
 }
 
 /**
@@ -80,11 +125,14 @@ function deltas(nfa, states, index) {
  * @param {ArrayLike<symbol>} states
  */
 function epsilonClosure(nfa, states) {
-  const worklist = Array.from(states);
   const seen = new Set(Array.from({ length: 0 }, () => Symbol("")));
-  
-  while (worklist.length > 0) {
-    const state1 = worklist.pop();
+  const worklist = Array.from(states);
+  let worklistIndex = 0;
+
+  while (worklist.length > worklistIndex) {
+    const state1 = worklist.at(worklistIndex);
+    worklistIndex++;
+
     const next = nfa.delta(state1, KnownMappings.epsilon);
     seen.add(state1);
     
@@ -101,3 +149,26 @@ function epsilonClosure(nfa, states) {
   return seen;
 }
 
+/**
+ * @param {Set<any>} set1
+ * @param {Set<any>} set2
+ */
+function subsetOf(set1, set2) {
+  for (const elem of set1) {
+    if (!set2.has(elem)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/**
+ * @param {Set<any>} set1
+ * @param {Set<any>} set2
+ */
+function addTo(set1, set2) {
+  for (const elem of set1) {
+    set2.add(elem);
+  }
+}
