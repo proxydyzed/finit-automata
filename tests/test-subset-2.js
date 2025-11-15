493d33ccbf2188069e21a1540d1400d05942d259
proxydyzed <tonnoysarker2@gmail.com>
2025-11-15 21:27:31 +0600

    better minimizer

--- tests/test-subset-2.js
+++ tests/test-subset-2.js
@@ -1,40 +1,134 @@
-import { FixedColumnTable } from "./tables.js";
-import { NondeterministicFiniteAutomata, KnownMappings } from "./nfa.js";
+import { NondeterministicFiniteAutomata } from "./nfa.js";
 import { DeterministicFiniteAutomata } from "./dfa.js";
-import { FiniteAutomata, ErrorState, ExhaustiveRecognizer } from "../dst/export.js";
-import { subsetConstruction, EmptySet, setsAreEqual, unionOfSets } from "./subset-construction.js";
+import { ErrorState, ExhaustiveRecognizer, KnownMappings } from "../dst/export.js";
+import { subsetConstruction } from "./subset-construction.js";
 
-class PartitionEntry {
+class MinimizationContext {
+  #dfa;
+  names;
+  worklist;
+  reachable;
+  partitions;
+
+  constructor(dfa) {
+    this.dfa = dfa;
+    this.names = new Map(Array.from({ length: 0 }, () => [
+      // nfa state
+      Symbol(""),
+      // partition array index
+      0,
+    ]));
+    this.worklist = new WorkList([]);
+    this.reachable = new Map(Array.from({ length: 0 }, () => [
+      // partition name
+      Symbol(""),
+      // partition array index
+      new Set(Array.from({ length: 0 }, () => 0)),
+    ]));
+    this.partitions = Array.from({ length: 0 }, () => new Partition(Symbol(""), new Set()));
+  }
+
+  get dfa() {
+    return this.#dfa;
+  }
+
+  set dfa(dfa) {
+    this.#dfa = dfa;
+  }
+
+  static from(dfa, partitions) {
+    const ctx = new this(dfa);
+
+    for (const states of partitions) {
+      ctx.addPartition(states);
+    }
+
+    return ctx;
+  }
+
+  addPartition(states) {
+    const index = this.partitions.length;
+    const entry = new Partition(Symbol(`p${index}`), states);
+    this.reachable.set(entry.name, new Set());
+
+    for (const state of states) {
+      this.names.set(state, index);
+    }
+
+    this.partitions.push(entry);
+    this.worklist.add(index);
+  }
+
+  addReachable(name, index) {
+    if (typeof name !== "symbol") {
+      throw new TypeError(`Expected argument "name" to be symbol, but got ${typeof name === "object" ? (name.constructor?.name ?? "null") : typeof name}`);
+    }
+    if (typeof index !== "number") {
+      throw new TypeError(`Expected argument "index" to be number, but got ${typeof index === "object" ? (index.constructor?.name ?? "null") : typeof name}`);
+    }
+    
+    this.reachable.get(name).add(index);
+  }
+
+  pushReachables(name) {
+    const reachable = this.reachable.get(name);
+    for (const reach of reachable) {
+      if (!this.worklist.has(reach)) {
+        this.worklist.add(reach);
+      }
+    }
+
+    reachable.clear();
+  }
+}
+
+class Partition {
   name;
   states;
-  members;
-  
+
   /**
    * @param {symbol} name
    * @param {Set<symbol>} states
-   * @param {symbol[]} members
    */
-  constructor(name, states, members) {
+  constructor(name, states) {
     this.name = name;
     this.states = states;
-    this.members = members;
   }
   
   toString() {
-    return `PartitionEntry{ name: ${this.name.description}, states: Set{${Array.from(this.states, state => state.description).join()}}, members: [${this.members.map(member => member.description).join()}] }`;
+    return `Partition{ name: ${this.name.description}, states: Set{${Array.from(this.states, state => state.description).join()}} }`;
+  }
+}
+
+class WorkList {
+  buffer;
+  index = 0;
+
+  constructor(buffer) {
+    this.buffer = buffer;
+  }
+
+  *[Symbol.iterator]() {
+    while (this.buffer.length > this.index) {
+      this.index++;
+      yield this.buffer.at(this.index - 1);
+    }
+  }
+
+  has(value) {
+    return this.buffer.includes(value, this.index);
+  }
+
+  add(value) {
+    this.buffer.push(value);
   }
 }
 
 try {
-  const nfa = setup1();
-  // console.log(JSON.stringify(nfa, null, 4));
-  // console.log(nfa.stringifyMappings());
-
-  const dfa = subsetConstruction(nfa);
-  // const dfa = setup3();
+  const dfa = setup2();
   const recognizer = new ExhaustiveRecognizer(dfa);
   
-  console.assert(recognizer.accepts("a"),   `Failed to recognize "a"`);
+  // console.assert(recognizer.accepts("a"),   `Failed to recognize "a"`);
   // console.log(dfa);
   // console.assert(recognizer.accepts("fie"),   `Failed to recognize "fie"`);
   // console.assert(recognizer.accepts("fee"),   `Failed to recognize "fee"`);
@@ -42,104 +136,19 @@ try {
   // console.assert(!recognizer.accepts("feee"), `Failed to recognize "feee"`);
   // console.assert(!recognizer.accepts("fii"),  `Failed to recognize "fii"`);
 
-  const dfa2 = minimizeDfa2(dfa);
+  // console.log(dfa);
+
+  const context = minimizeDfa2(dfa);
+  console.log(context);
+  console.log(context.partitions.join("\n"));
 } catch (error) {
   console.error(error.stack);
 }
-
-
-/*
-
-for (alpha of alphabets):
-  iteration(f):
-    for (part of partition):
-      iteration({ s3, s5 }):
-        for (state of { s3, s5 }):
-          s3 + f = 0;
-          s5 + f = 0;
-        end;
-      end;
-      iteration({ s0, s1, s2, s4 }):
-        for (state of { s0, s1, s2, s4 }):
-          s0 + f = s1;
-          s1 + f = 0;
-          s2 + f = 0;
-          s4 + f = 0;
-        end;
-      end;
-    end;
-  end;
-
-  iteration(e):
-    for (part of partition):
-      iteration({ s3, s5 }):
-        for (state of { s3, s5 }):
-          s3 + e = 0;
-          s5 + e = 0;
-        end;
-      end;
-      iteration({ s0, s1, s2, s4 }):
-        for (state of { s0, s1, s2, s4 }):
-          s0 + e = 0;
-          s1 + e = s2; // partition 1
-          s2 + e = s3; // partition 0
-          s4 + e = s5; // partition 0
-        end;
-      end;
-    end;
-  end;
-end;
-
-alpha(f):
-  for (part of partion):
-    iteration(1):
-      s3 + f = 0
-      s5 + f = 0
-    end;
-
-    iteration(2):
-      s0 + f = s1
-      s1 + f = 0
-      s2 + f = 0
-      s4 + f = 0
-    end;
-  end;
-end;
-
-alpha(e):
-  for (part of partition):
-    iteration(1):
-      s3 + e = 0
-      s5 + e = 0
-    end;
-
-    iteration(2):
-      s0 + e = 0
-      s1 + e = s2
-      s2 + e = s3 // offending
-      s4 + e = s5 // offending
-    end;
-
-    iteration(n):
-      image := part[n] + alpha(@)
-      for (part2 of partition):
-        if (part2.has(image)):
-          partitionNames.get(part2).push(image);
-        end;
-      end;
-    end;
-  end;
-end;
-
-
-*/
 
 /**
  * @param {DeterministicFiniteAutomata} dfa
  */
 function minimizeDfa2(dfa) {
-  const dfa2 = new DeterministicFiniteAutomata(Symbol("start"));
-
   const accepting = dfa.accepting;
   const nonAccepting = new Set(Array.from({ length: 0 }, () => Symbol("")));
 
@@ -148,14 +157,110 @@ function minimizeDfa2(dfa) {
       nonAccepting.add(state);
     }
   }
-  
-  try {
-  const worklist = [accepting, nonAccepting];
-  let partitionEntries = worklist.map((workset, i) => new PartitionEntry(Symbol(`m${i}`), workset, []));
-  console.log(partitionEntries.join("\n"));
-  
-  } catch (error) {
-    console.log(error.stack);
+
+  const context = MinimizationContext.from(dfa, [accepting, nonAccepting]);
+  for (const index of context.worklist) {
+    processWorkset(context, context.partitions.at(index), index);
+  }
+
+  return context;
+}
+
+/**
+ * @param {MinimizationContext} ctx
+ * @param {Partition} workset
+ * @param {number} partitionIndex
+ */
+function processWorkset(ctx, workset, partitionIndex) {
+  const { dfa } = ctx;
+  // console.log(`Partition-index: ${partitionIndex}`);
+  // console.log("Partition-entry:", String(workset));
+  for (const [alpha, index] of dfa.alphabets) {
+    processAlpha(ctx, workset, partitionIndex, alpha, index);
+  }
+  // console.log("\n");
+}
+
+/**
+ * @param {MinimizationContext} ctx
+ * @param {Partition} workset
+ * @param {number} partitionIndex
+ * @param {string} alpha
+ * @param {number} index
+ */
+function processAlpha(ctx, workset, partitionIndex, alpha, index) {
+  const { dfa, partitions, names } = ctx;
+  const bucket = new Map();
+  const errorBucket = [];
+  // console.log(`Processing: "${alpha}"`);
+
+  for (const state of workset.states) {
+    const image = dfa.delta(state, index);
+    // console.log(`  ${state.description} + "${alpha}" => ${image.description}`);
+
+    if (image !== ErrorState) {
+      const partition = partitions.at(names.get(image));
+      // partition "A" is reaching some partition "B".
+      // we add this so that later if we split the
+      // partition "B" then we can add partition "A"
+      // to the worklist as the entries in partition "A"
+      // might reach any of the part of the split up
+      // partition "B"
+      ctx.addReachable(partition.name, partitionIndex);
+      //               ^ partition "B"
+      //                               ^ partition "A"
+
+      // console.log("Partition:", partition.toString());
+
+      if (bucket.has(partition.name)) {
+        bucket.get(partition.name).push(state);
+      } else {
+        bucket.set(partition.name, [state]);
+      }
+    } else {
+      errorBucket.push(state);
+    }
+  }
+
+  if (errorBucket.length > 0) {
+    bucket.set(ErrorState, errorBucket);
+  }
+
+  // console.log("Reachable:");
+  // console.log(Array.from(reachable, ([k, v]) => `  ${k.description} => Set{${Array.from(v).join(", ")}}`).join("\n"));
+  // console.log("Bucket:");
+  // console.log(Array.from(bucket, ([k, v]) => `  ${k.description} => [${Array.from(v, s => s.description).join(", ")}]`).join("\n"));
+  // console.log("");
+
+  if (bucket.size > 1) {
+    const partition = partitions.at(partitionIndex);
+
+    const temp = [];
+    for (const [, states] of bucket) {
+      temp.push(new Set(states));
+    }
+    // console.log("Split states:");
+    // console.log(temp);
+
+    // console.log("Partition to split:");
+    // console.log(String(partition));
+
+    partition.states = temp.pop();
+    for (const state of partition.states) {
+      names.set(state, partitionIndex);
+    }
+
+    // console.log("Partition is split:");
+    // console.log(String(partition));
+
+    for (const states of temp) {
+      ctx.addPartition(states);
+    }
+
+    // we push all partition "A" reachable from
+    // partition "B" to the worklist and clear
+    // the reachable set
+    ctx.pushReachables(partition.name);
   }
 }
 
@@ -251,40 +356,40 @@ function setup1() {
 
   nfa.accepting.add(n9);
 
-  return nfa;
+  return subsetConstruction(nfa);
 }
 
 function setup2() {
-  const nfa = new NondeterministicFiniteAutomata("a");
+  const dfa = new DeterministicFiniteAutomata("a");
 
-  const na = nfa.start;
-  const nb = nfa.addVertex("b");
-  const nc = nfa.addVertex("c");
-  const nd = nfa.addVertex("d");
-  const ne = nfa.addVertex("e");
-  const nf = nfa.addVertex("f");
+  const na = dfa.start;
+  const nb = dfa.addVertex("b");
+  const nc = dfa.addVertex("c");
+  const nd = dfa.addVertex("d");
+  const ne = dfa.addVertex("e");
+  const nf = dfa.addVertex("f");
 
-  const a0 = nfa.addAlphabet("0");
-  const a1 = nfa.addAlphabet("1");
+  const a0 = dfa.addAlphabet("0");
+  const a1 = dfa.addAlphabet("1");
 
-  nfa.addEdge(a1, na, nc);
-  nfa.addEdge(a0, na, nb);
-  nfa.addEdge(a0, nb, na);
-  nfa.addEdge(a1, nb, nd);
-  nfa.addEdge(a0, nc, ne);
-  nfa.addEdge(a1, nc, nf);
-  nfa.addEdge(a0, nd, ne);
-  nfa.addEdge(a1, nd, nf);
-  nfa.addEdge(a0, ne, ne);
-  nfa.addEdge(a1, ne, nf);
-  nfa.addEdge(a0, nf, nf);
-  nfa.addEdge(a1, nf, nf);
+  dfa.addEdge(a1, na, nc);
+  dfa.addEdge(a0, na, nb);
+  dfa.addEdge(a0, nb, na);
+  dfa.addEdge(a1, nb, nd);
+  dfa.addEdge(a0, nc, ne);
+  dfa.addEdge(a1, nc, nf);
+  dfa.addEdge(a0, nd, ne);
+  dfa.addEdge(a1, nd, nf);
+  dfa.addEdge(a0, ne, ne);
+  dfa.addEdge(a1, ne, nf);
+  dfa.addEdge(a0, nf, nf);
+  dfa.addEdge(a1, nf, nf);
 
-  nfa.accepting.add(nc);
-  nfa.accepting.add(nd);
-  nfa.accepting.add(ne);
+  dfa.accepting.add(nc);
+  dfa.accepting.add(nd);
+  dfa.accepting.add(ne);
 
-  return nfa;
+  return dfa;
 }
 
 function setup3() {
@@ -312,24 +417,4 @@ function setup3() {
   dfa.accepting.add(s5);
 
   return dfa;
-}
-
-function intersection(set1, set2) {
-  const set = new Set();
-  for (const elem of set1) {
-    if (set2.has(elem)) {
-      set.add(elem);
-    }
-  }
-
-  return set;
-}
-
-function subtraction(set1, set2) {
-  const set = new Set(set1);
-  for (const elem of set2) {
-    set.delete(elem);
-  }
-
-  return set;
 }
