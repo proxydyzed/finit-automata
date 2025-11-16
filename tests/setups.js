1fd21b1c006b11698f96d69ac160579058e3809c
proxydyzed <tonnoysarker2@gmail.com>
2025-11-17 02:11:02 +0600

    removed broken examples

--- tests/setups.js
+++ tests/setups.js
@@ -5,6 +5,7 @@ import {
 } from "../dst/export.js";
 import {
   nfa2dfa,
+  minimizeDfa,
 } from "../algs/export.js";
 
 export function setup1() {
@@ -155,6 +156,31 @@ export function setup6() {
   console.log(nfa.stringifyMappings());
   
   return nfa2dfa(nfa);
+}
+
+export function setup7() {
+  // nearly irreducible
+  const nfa = new NondeterministicFiniteAutomata("q0");
+
+  const q0 = nfa.start;
+  const q1 = nfa.addVertex("q1");
+  const q2 = nfa.addVertex("q2");
+  const q3 = nfa.addVertex("q3");
+  const q4 = nfa.addVertex("q4");
+
+  const a = nfa.addAlphabet("a");
+  const b = nfa.addAlphabet("b");
+
+  nfa.addEdge(a, q0, q1);
+  nfa.addEdge(KnownMappings.epsilon, q0, q3);
+  nfa.addEdge(b, q1, q2);
+  nfa.addEdge(KnownMappings.sigma, q2, q0);
+  nfa.addEdge(KnownMappings.sigma, q3, q4);
+  nfa.addEdge(b, q4, q3);
+  nfa.accepting.add(q1);
+  nfa.accepting.add(q3);
+
+  return nfa;
 }
 
 export function buildString(nfa, str) {
--- tests/test-setups.js
+++ tests/test-setups.js
@@ -7,7 +7,6 @@ import {
 } from "../dst/export.js";
 import {
   minimizeDfa,
-  subsetConstruction,
 } from "../algs/export.js";
 import * as setups from "./setups.js";
 
@@ -21,13 +20,6 @@ try {
   for (const { input, output } of entries) {
     console.assert(output === recognizer.accepts(input), `Failed to recognize "${input}"`);
   }
-
-  // console.log(dfa);
-  // console.log(dfa.stringifyMappings())
-
-  const context = minimizeDfa(dfa);
-  // console.log(context);
-  console.log(context.partitions.join("\n"));
 } catch (error) {
   console.log(error.message)
   console.log(error.stack);
